b3951a9860c9305c8037906097a6a6a4754c2278
Oleh Kozynets <OlehKSS@users.noreply.github.com>
2018-04-27 22:53:43 +0200

    Opencv maven artifact was added, fixes #4. (#5)

--- src/main/java/it/unicas/App.java
+++ src/main/java/it/unicas/App.java
@@ -1,5 +1,10 @@
 package it.unicas;
 
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_highgui;
+//import org.bytedeco.javacpp.opencv_imgproc;
+import org.bytedeco.javacpp.opencv_imgcodecs;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        opencv_core.Mat img = opencv_imgcodecs.imread("dataset/images/20586908_6c613a14b80a8591_MG_R_CC_ANON.tif",
+                opencv_imgcodecs.IMREAD_GRAYSCALE & opencv_imgcodecs.IMREAD_ANYDEPTH);
+
+        img.convertTo(img, opencv_core.CV_8U);
+
+
+        opencv_highgui.namedWindow("test", opencv_highgui.WINDOW_NORMAL);
+        opencv_highgui.imshow("test", img);
+        opencv_highgui.waitKey(0);
         System.out.println( "Hello World!" );
     }
 }
