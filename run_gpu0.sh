5d55becc189c6a8b44292f12dfb2704862043085
OlehKSS <olehkss@github.com>
2018-05-14 10:36:15 +0200

    Java vm execution

--- run_gpu0.sh
+++ run_gpu0.sh
@@ -6,4 +6,6 @@ cp ./configurations/pom-gpu.xml pom.xml
 
 # building and running
 mvn compile
-mvn exec:java -Dexec.mainClass=it.unicas.App
+#mvn exec:java -Dexec.mainClass=it.unicas.App
+
+java -cp ./target/automated-mass-detection-1.0.jar it.unicas.App
