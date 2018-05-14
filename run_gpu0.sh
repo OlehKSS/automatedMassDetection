ee46b9677230bd5fa905f9bbb5a72c92c5e55686
OlehKSS <olehkss@github.com>
2018-05-14 11:42:38 +0200

    permissions for running

--- run_gpu0.sh
+++ run_gpu0.sh
@@ -4,8 +4,12 @@ export CUDA_VISIBLE_DEVICES=0
 rm ./pom.xml
 cp ./configurations/pom-gpu.xml pom.xml
 
-# building and running
+# building and running on maven
 mvn compile
-#mvn exec:java -Dexec.mainClass=it.unicas.App
+mvn exec:java -e -Dexec.mainClass=it.unicas.App
 
-java -cp ./target/automated-mass-detection-1.0.jar it.unicas.App
+# running on java
+#mvn clean install
+#mvn verify
+
+#java -cp ./target/automated-mass-detection-1.0.jar it.unicas.App
